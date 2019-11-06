a16bcd5b4b26cb609943a1b28a124eb246254e98
kiryusha003 <57086801+kiryusha003@users.noreply.github.com>
2019-11-06 10:21:36 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,21 +1,7 @@
-
-# some_module.py
-"""Этот модуль содержит классы Class1 и Class2."""
-
 class Class1:
-    """Класс, выполняющий действия над матрицами."""
-    
-    def __init__(self, list):
-        """list -- список содержимого матрицы."""
-        pass
-        
-    def transpose():
-        """Метод транспонирует матрицу."""
-        pass
     def get_films_list():
       for item in parsed_main_page.find_all("div", class_='movie-plate'):
         a.append(item['attr-title'])
      return a
 get_films_list()
 
-# ...
