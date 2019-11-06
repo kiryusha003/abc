9c4cf4709fd681b81ac60fdeb2fb3a2c1a1e8e6f
kiryusha003 <57086801+kiryusha003@users.noreply.github.com>
2019-11-06 10:25:29 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup as bs
 import requests
 class CinemaParser:
+    def __init__ (self,city='msk'):
+        self.city=city
     def get_films_list():
       for item in parsed_main_page.find_all("div", class_='movie-plate'):
         a.append(item['attr-title'])
